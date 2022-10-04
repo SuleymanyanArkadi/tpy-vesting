6501d257619ab56d29c0f0ec3a32f2305c508c8c
arkadi.suleymanyan.s <arkadi.suleymanyan.s@gmail.com>
2022-10-04 14:49:12 +0400

    Fixes

--- contracts/mocks/TPYToken.sol
+++ contracts/mocks/TPYToken.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TPYToken is ERC20 {
     constructor() ERC20("TPY Token", "TPY") {
-        _mint(msg.sender, 1e9 * 1e8);
+        _mint(msg.sender, 1e9 * 1e18);
     }
 
     function mint(address account, uint256 amount) external {
