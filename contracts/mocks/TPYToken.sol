baabed6fec8dc41e6f8e4589080184cc71479dec
arkadi.suleymanyan.s <arkadi.suleymanyan.s@gmail.com>
2022-10-04 14:45:00 +0400

    Change TPY token decimal

--- contracts/mocks/TPYToken.sol
+++ contracts/mocks/TPYToken.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TPYToken is ERC20 {
     constructor() ERC20("TPY Token", "TPY") {
-        _mint(msg.sender, 1e9 * 1e18);
+        _mint(msg.sender, 1e9 * 1e8);
     }
 
     function mint(address account, uint256 amount) external {
